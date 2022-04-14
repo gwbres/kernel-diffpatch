480c92e043fcc52240eb310e417277ef602ee7ef
gwbres <guillaume.bressaix@gmail.com>
2022-04-14 17:00:04 +0200

    add current tree

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # Kernel diffpatch
-Scripts to generate patch series between two Kernel forks
+
+Scripts to generate patch series between two Kernel forks.
+
+Sometimes you want to retrieve custom features from a given fork
+and apply them to another. This patch generator helps you
+extract custom content and generate a patch serie that can be
+directly applied and built `in tree`.
+
+I personnaly use this script with Analog Devices's fork,
+which is usually merged into Xilinx's kernel.
+
+As an example, the makefile is set to 
+Analog Devices's repo and we generate a couple of patches to embed some of their custom drivers.  
+
+This assumes you have a local linux kernel `diff` with, specificed with `--repo /foo/bar`.   
+You can control the source repo, containing custom content with `--url https://foo/bar`.   
+You can use a specific remote branc with `--branch custom`.   
+Control the custom content to extract from the remote branch, with the `files.txt` listing.
+
+```shell
+# list of custom ADI's work
+# we would like to enhance our local repo with:
+cat files.txt 
+drivers/clk/adi/Kconfig
+drivers/clk/adi/Makefile
+drivers/clk/adi/clk-ad9545-i2c.c
+drivers/clk/adi/clk-ad9545-spi.c
+drivers/clk/adi/clk-ad9545.c
+drivers/clk/adi/clk-ad9545.h
+
+# let's generate a patch serie for those files
+╰─$ ./kernel.py --repo /tmp/linux-custom
+new patch : 0001-drivers-clk-adi-Kconfig.patch
+new patch : 0002-drivers-clk-adi-Makefile.patch
+new patch : 0003-drivers-clk-adi-clk-ad9545-i2c.patch
+new patch : 0004-drivers-clk-adi-clk-ad9545-spi.patch
+new patch : 0005-drivers-clk-adi-clk-ad9545.patch
+new patch : 0006-drivers-clk-adi-clk-ad9545.patch
+```
+
+Now apply custom features to enhance our local repo:
+```shell
+cd /tmp/linux-custom
+╰─$ cd /tmp/linux-custom
+╰─$ patch -p1 < $kernel-diffpatch/patches/*.patch
+```
+
+You don't have to `diff` with Analog Devices's fork, 
+you can use any remote linux kernel :
+```shell
+╰─$ ./kernel.py --repo /tmp/linux-custom \
+    --url https://github.com/xilinx/linux-xlnx
+```
+
+You can specify a non default branch to work with:  
+```shell
+╰─$ ./kernel.py --repo /tmp/linux-custom \
+    --url https://github.com/xilinx/linux-xlnx \
+        --tag 2020.01
+```
+
---
+++ kernel.py
@@ -0,0 +1,98 @@
+#! /usr/bin/env python3
+import os
+import sys
+import subprocess
+
+ENV = {
+    "url": "https://github.com/analogdevicesinc/linux",
+    "tag": "master",
+    "repo": None,
+}
+
+def command (cmd):
+    ret = os.system(cmd)
+    if ret > 0:
+        raise RuntimeError("failed to exec {}".format(cmd))
+def exists (path):
+    return os.path.exists(path)
+def git_clone (url, branch):
+    command("git clone {} --branch {}".format(url, branch))
+def mkdir (path):
+    command("mkdir -p {}".format(path))
+
+def file_suffix (path):
+    """ truncates x/y/path into "path" """
+    return path.split("/")[-1]
+def file_prefix (path):
+    """ truncates x/y/path into "path" """
+    return "/".join(path.split("/")[:-1])
+def strip_prefix (path):
+    """ truncates x/y/path.z into "path" """
+    return path.split("/")[-1].split(".")[0]
+def strip_extension (path):
+    """ truncates x/y/path.z into "z" """
+    return path.split("/")[-1].split(".")[-1]
+
+def main (argv):
+    global ENV
+
+    for i in range (0, len(argv)):
+        key = argv[i].strip("--")
+        if key in ENV:
+            ENV[key] = argv[i+1]
+    
+    # env
+    repo = ENV["repo"]
+    url  = ENV["url"]
+    tag  = ENV["tag"]
+
+    # download custom content 
+    clone_target = url.split("/")[-1]
+    if not(exists(clone_target)):
+        git_clone(url, tag)
+
+    # build products
+    mkdir("patches")
+    # parse products
+    with open("files.txt", "r") as fd:
+        files = fd.read().strip().split("\n")
+ 
+    # run diff for all selected files
+    count = 1
+    for f in files:
+        prefix = file_prefix(f).replace("/", "-")
+        prefix = prefix.replace("_","-")
+        name = strip_prefix(f).replace("_","-")
+        patch = "{:04d}-{}-{}.patch".format(count, prefix, name)
+        # two options
+        # [1] either this file exists 
+        #     --> custom modification
+        # [2] or this file does not exist in local repo
+        #     --> brand new
+        if exists(repo +"/" +f):
+            # patch to generate
+            print("modification patch : \"{}\"".format(patch))
+        else:
+            # patch to generate
+            print("new patch : {}".format(patch))
+            cmd = [
+                "diff", 
+                "-ruN", 
+                "{}/{}".format(repo, f),
+                "{}/{}".format(clone_target, f),
+            ]
+            # run `diff`
+            ret = subprocess.run(cmd, capture_output=True)
+            stdout = ret.stdout.decode("utf-8")
+            # need to replace, in generated patch
+            # the /x/y/z prefix of the local repo,
+            # so this patch can apply in tree
+            stdout = stdout.replace("{}/{}".format(repo, f), "null")
+            # produce patch  
+            with open("patches/{}".format(patch), "w") as fd:
+                fd.write(stdout)
+
+        count += 1
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
